36ab7e8fab3c1634629e205656c34a858da0382a
sskohli <sskohli79@gmail.com>
2020-06-08 16:46:33 +0530

    first portfolio

---
+++ react-ux/1/precache-manifest.a7682be3ded57edac5a95e2bae7f63e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3753b11345b510a18f150f7ff351a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b806406571d28eed3678",
+    "url": "/static/css/main.405abd05.chunk.css"
+  },
+  {
+    "revision": "41e632765c5618cd7233",
+    "url": "/static/js/2.61b247fa.chunk.js"
+  },
+  {
+    "revision": "b95b91deaf333be79aa97a8a6d93da35",
+    "url": "/static/js/2.61b247fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b806406571d28eed3678",
+    "url": "/static/js/main.02b6aa23.chunk.js"
+  },
+  {
+    "revision": "6835862235c63fa3e33c",
+    "url": "/static/js/runtime-main.760f5555.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
